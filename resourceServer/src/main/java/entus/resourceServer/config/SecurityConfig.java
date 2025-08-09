24439a73cbef350ee2351f8c96e8b31a20355d80
KTH <taehyeonentus@gmail.com>
2025-08-09 19:56:35 +0900

    update to global handler from denied handler, entry point control

--- resourceServer/src/main/java/entus/resourceServer/config/SecurityConfig.java
+++ resourceServer/src/main/java/entus/resourceServer/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package entus.resourceServer.config;
 
-import entus.resourceServer.exception.CustomAccessDeniedHandler;
-import entus.resourceServer.exception.CustomAuthenticationEntryPoint;
 import entus.resourceServer.filter.token.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -18,8 +16,6 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final CustomAccessDeniedHandler customAccessDeniedHandler;
-    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -31,10 +27,7 @@ public class SecurityConfig {
                         .requestMatchers("/user").hasRole("user")
                         .requestMatchers("/admin").hasRole("admin")
                         .anyRequest().authenticated())
-                .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter.class)
-                .exceptionHandling(ex -> ex
-                        .authenticationEntryPoint(customAuthenticationEntryPoint)
-                        .accessDeniedHandler(customAccessDeniedHandler));
+                .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter.class);
 
         return http.build();
     }
